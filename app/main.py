d9a5fcf2131da5d170c6f036ff9677352de9005b
Gabriel Carlos <gabrielcarlosoliveira0@gmail.com>
2025-04-02 11:24:13 -0300

    Change requirements DB and Test new route Get methods

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
 """API python com flask."""
 
-from flask import Flask
+# Importa bibliotecas importante para API como request para requisições e jsonify para converter os dados em json
+from flask import Flask, request, jsonify
+
+# Importa biblioteca do Banco de dados
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# Cria a rota /
 @app.route("/")
 def hello_world():
     """Função para retorna uma string na rota / da API."""
     return "<p>Hello, World!</p>"
+
+# Cria a rota test que 
+@app.route('/test', methods=['GET'])
+def getTest():
+    name = request.args.get('name', 'Gabriel')
+    return f'Hello, {name}!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
